f4f30022910dda98afe3e9a6ac3ffefae28a0248
div1103 <div110393@gmail.com>
2021-03-17 15:54:03 +0300

    add function create_database

--- AddUsers.py
+++ AddUsers.py
@@ -2,13 +2,14 @@ import mysql.connector
 from mysql.connector import Error
 
 
-def create_connection(host_name, user_name, user_password):
+def create_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
+            passwd=user_password,
+            database=db_name,
         )
         print("Connection to MySQL DB successful")
     except Error as e:
@@ -17,4 +18,18 @@ def create_connection(host_name, user_name, user_password):
     return connection
 
 
-connection = create_connection("localhost", "root", "Stolovichi93")
+connection = create_connection(
+    "localhost", "root", "Stolovichi93", "user_role")
+
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+
+create_database_query = "CREATE DATABASE user_role"
+create_database(connection, create_database_query)
